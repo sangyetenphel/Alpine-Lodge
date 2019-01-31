972a001b7afca15d6a10918aef5155f6cfb78a98
SangyeTenphel <46420445+SangyeTenphel@users.noreply.github.com>
2019-01-31 21:16:08 +0530

    alpine lodge full project

---
+++ alpine(online)/footer.php
@@ -0,0 +1,12 @@
+<section id="footer-menus-wrapper">
+		For reservations, call (740) 724-4766
+	</section>
+
+    <footer>
+		<div class="alignleft">
+			Alpine Lodge, Copyright &copy; 2018
+		</div>
+		<div class="alignright">
+			This website was designed by Sangye Tenphel
+		</div>
+    </footer>
